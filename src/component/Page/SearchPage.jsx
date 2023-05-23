65fc49362d321ed4697c5d31c09b6e6893dc09ed
jaeochoiii <jaeochoiii0716@inha.edu>
2023-05-23 22:28:43 +0900

    fix.SearchPage 인기검색어

--- src/component/Page/SearchPage.jsx
+++ src/component/Page/SearchPage.jsx
@@ -40,6 +40,21 @@ const Text = styled.div`
   padding-left: 17px;
 `;
 
+const PopularBox = styled.div`
+  width: 340px;
+  height: 30px;
+  @import url("https://fonts.googleapis.com/css2?family=Mogra&display=swap");
+  font-family: "Mogra", cursive;
+  font-weight: 700;
+  font-size: 15px;
+  border-bottom: 1px solid #eeeded;
+  letter-spacing: 1px;
+  margin-top: 10px;
+  margin-left: 17px;
+  display: flex;
+  align-items: end;
+`;
+
 const SearchPage = () => {
   return (
     <>
@@ -48,6 +63,13 @@ const SearchPage = () => {
         <TextWrapper>Search</TextWrapper>
         <SearchBox placeholder="검색어를 입력해주세요" />
         <Text>인기검색어</Text>
+        <PopularBox>1. 런던 베이글 뮤지엄</PopularBox>
+        <PopularBox>2. 너티드 도넛</PopularBox>
+        <PopularBox>3. 쉐이크 쉑</PopularBox>
+        <PopularBox>4. 마카롱</PopularBox>
+        <PopularBox>5. 블루보틀</PopularBox>
+        <PopularBox>6. 랜디스 도넛</PopularBox>
+        <PopularBox>7. 다운타우너 버거</PopularBox>
       </BodyContent>
       <Footer />
     </>
