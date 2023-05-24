1981cbb2b1a826be95c78ee66c7e08f18e1e8953
jaeochoiii <jaeochoiii0716@inha.edu>
2023-05-24 20:02:03 +0900

    feat. Heart Button 구현

--- src/component/Page/HeartPage.jsx
+++ src/component/Page/HeartPage.jsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import Header from "../../uiPage/Header";
 import Footer from "../../uiPage/Footer";
 import Liked from "../../assets/LikedIMG.png";
+import { AiFillHeart } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+import { useState } from "react";
 
 const BodyContent = styled.div`
   width: 390px;
@@ -29,6 +32,10 @@ const Text = styled.div`
   padding-left: 17px;
 `;
 
+const ImgDiv = styled.div`
+  width: 350px;
+  height: 250px;
+`;
 const ImgWrapper = styled.img`
   display: flex;
   width: 319px;
@@ -37,12 +44,25 @@ const ImgWrapper = styled.img`
   padding-left: 30px;
   padding-top: 8px;
 `;
+const DivWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f6f2dd;
+  width: 35px;
+  height: 35px;
+  border: 2px solid black;
+  border-radius: 50px;
+  margin-left: 310px;
+  margin-top: 4px;
+`;
+
 const InformBox = styled.div`
   width: 320px;
   height: 30px;
   display: flex;
   justify-content: space-between;
-  padding-top: 6px;
+  padding-top: 35px;
   padding-left: 30px;
 `;
 const TitleText = styled.div`
@@ -60,16 +80,31 @@ const PriceText = styled.div`
 `;
 
 const HeartPage = () => {
+  const [heart, setHeart] = useState(false);
+
+  const handleHeart = () => {
+    setHeart(!heart);
+  };
+
   return (
     <>
       <Header />
       <BodyContent>
         <TextWrapper>Liked</TextWrapper>
         <Text>내가 찜한 상품</Text>
-        <ImgWrapper src={Liked} />
+        <ImgDiv>
+          <ImgWrapper src={Liked} />
+          <DivWrap onClick={handleHeart}>
+            {heart ? (
+              <AiFillHeart style={{ color: "red", fontSize: "30px" }} />
+            ) : (
+              <AiOutlineHeart style={{ fontSize: "30px" }} />
+            )}
+          </DivWrap>
+        </ImgDiv>
         <InformBox>
-          <TitleText>Naughted Donut Set</TitleText>
-          <PriceText>13900원</PriceText>
+          <TitleText>Knotted Donut Set</TitleText>
+          <PriceText>13900 Won</PriceText>
         </InformBox>
       </BodyContent>
       <Footer />
