b5e97b2b42f0a57ac57028b0d794ef11b3c70f03
jaeochoiii <jaeochoiii0716@inha.edu>
2023-05-28 13:28:32 +0900

    feat.MenuPage Framing

--- src/component/MenuPage.jsx
+++ src/component/MenuPage.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import Header from "../uiPage/Header";
 import Footer from "../uiPage/Footer";
+import { RiArrowDownSLine } from "react-icons/ri";
+import Coffee from "../assets/coffee.png";
+import Bakery from "../assets/bakery.png";
+import Macaron from "../assets/macaron.png";
+import Pestury from "../assets/pastury.png";
+import Cake from "../assets/cake.png";
+import Bagle from "../assets/bagle.png";
+import Candy from "../assets/candy.png";
+import Cookie from "../assets/cookie.png";
+import Sandwich from "../assets/sandwitch.png";
 
 const BodyContent = styled.div`
   width: 390px;
@@ -16,7 +26,15 @@ const TextWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding-top: 30px;
+  padding-top: 20px;
+`;
+
+const CategoryWrapper = styled.div`
+  display: flex;
+  width: 350px;
+  height: 40px;
+  border-bottom: 1px solid #cecece;
+  margin-left: 22px;
 `;
 
 const MenuPage = () => {
@@ -25,6 +43,16 @@ const MenuPage = () => {
       <Header />
       <BodyContent>
         <TextWrapper>Category</TextWrapper>
+        <CategoryWrapper></CategoryWrapper>
+        <CategoryWrapper></CategoryWrapper>
+        <CategoryWrapper></CategoryWrapper>
+        <CategoryWrapper></CategoryWrapper>
+        <CategoryWrapper></CategoryWrapper>
+        <CategoryWrapper></CategoryWrapper>
+        <CategoryWrapper></CategoryWrapper>
+        <CategoryWrapper></CategoryWrapper>
+        <CategoryWrapper></CategoryWrapper>
+        <CategoryWrapper></CategoryWrapper>
       </BodyContent>
       <Footer />
     </>
