91cdf369a55cc84655a0d0d9ca3d00f7affb0419
jaeochoiii <jaeochoiii0716@inha.edu>
2023-05-30 15:41:32 +0900

    feat.ManagerPage Profile img

--- src/component/Page/ManagerPage.jsx
+++ src/component/Page/ManagerPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Profile from "../../assets/profile.png";
 import Header from "../../uiPage/Header";
 import Footer from "../../uiPage/Footer";
 
@@ -19,12 +20,21 @@ const TextWrapper = styled.div`
   padding-top: 20px;
 `;
 
+const ProfileWrapper = styled.img`
+  display: flex;
+  padding-top: 20px;
+  padding-left: 155px;
+  width: 80px;
+  height: 80px;
+`;
+
 const ManagerPage = () => {
   return (
     <>
       <Header />
       <BodyContent>
-        <TextWrapper></TextWrapper>
+        <TextWrapper>Manage</TextWrapper>
+        <ProfileWrapper src={Profile} />
       </BodyContent>
       <Footer />
     </>
