be1a2cbaa21e74c7cc91fac6a873df0c045938bd
jaeochoiii <jaeochoiii0716@inha.edu>
2023-05-31 20:14:45 +0900

    feat.DBTable

--- src/component/Page/UserManage.jsx
+++ src/component/Page/UserManage.jsx
@@ -19,12 +19,60 @@ const TextWrapper = styled.div`
   padding-top: 20px;
 `;
 
+const UserState = styled.div`
+  width: 330px;
+  height: 155px;
+  border: 1px solid black;
+  border-radius: 7px;
+  margin-top: 31px;
+  margin-left: 30px;
+`;
+
+const UserText = styled.div`
+  width: 330px;
+  height: 30px;
+  border-bottom: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const IdText = styled.div`
+  width: 50px;
+  height: 12px;
+  font-size: 13px;
+  padding-left: 20px;
+`;
+
+const IdText2 = styled.div`
+  width: 50px;
+  height: 12px;
+  font-size: 13px;
+  padding-left: 10px;
+`;
+
 const UserManage = () => {
   return (
     <>
       <Header />
       <BodyContent>
         <TextWrapper>User Manage</TextWrapper>
+        <UserState>
+          <UserText>
+            <IdText>아이디</IdText>
+            <IdText>이름</IdText>
+            <IdText>비밀번호</IdText>
+            <IdText>상태</IdText>
+          </UserText>
+          <UserText>
+            <IdText2>inha2023</IdText2>
+            <IdText2>최재오</IdText2>
+            <IdText2>internet23!</IdText2>
+            <IdText2>관리자</IdText2>
+          </UserText>
+          <UserText></UserText>
+          <UserText></UserText>
+        </UserState>
       </BodyContent>
       <Footer />
     </>
