a03214a1312474a8ba7e7f82c9b018703d713a6b
jaeochoiii <jaeochoiii0716@inha.edu>
2023-06-07 14:33:56 +0900

    feat. PurchasePage Framing

---
+++ src/component/Page/PurchasePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../../uiPage/Header";
+import Footer from "../../uiPage/Footer";
+
+const PurchasePage = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default PurchasePage;
