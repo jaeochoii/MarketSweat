6de5c67d12b82d17cf4160aa8d33f25a1b4e81bf
jaeochoiii <jaeochoiii0716@inha.edu>
2023-06-07 15:03:49 +0900

    feat. Purchase Page 버튼 및 텍스트 작성 완료

--- src/component/Page/PurchasePage.jsx
+++ src/component/Page/PurchasePage.jsx
@@ -26,6 +26,7 @@ const DateText = styled.div`
   font-family: "Miriam Libre", sans-serif;
   font-weight: 700;
   font-size: 17px;
+  color: #cecece;
   border-bottom: 2px solid #cecece;
   width: 350px;
   height: 20px;
@@ -40,7 +41,6 @@ const PurchaseText = styled.div`
   font-family: "Miriam Libre", sans-serif;
   font-weight: 700;
   font-size: 18px;
-  color: #cecece;
   width: 350px;
   height: 20px;
   padding-top: 15px;
@@ -54,7 +54,37 @@ const RecContent = styled.img`
   border: 1px solid #d8cb93;
   border-radius: 2px;
   margin-left: 20px;
-  margin-top: 15px;
+  margin-top: 10px;
+`;
+
+const PurchaseDiv = styled.div`
+  display: flex;
+  width: 300px;
+  height: 150px;
+  padding-top: 10px;
+`;
+
+const RecText = styled.div`
+  display: flex;
+  @import url("https://fonts.googleapis.com/css2?family=Miriam+Libre&display=swap");
+  font-family: "Miriam Libre", sans-serif;
+  font-weight: 700;
+  font-size: 15px;
+  width: 150px;
+  padding-left: 20px;
+  padding-top: 10px;
+`;
+
+const PurchaseBut = styled.button`
+  width: 350px;
+  height: 34px;
+  border: 1px solid #f6f2dd;
+  background-color: #f6f2dd;
+  border-radius: 3px;
+  font-size: 15px;
+  font-weight: 700;
+  margin-top: 10px;
+  margin-left: 20px;
 `;
 
 const PurchasePage = () => {
@@ -65,7 +95,22 @@ const PurchasePage = () => {
         <TextWrapper>Purchase</TextWrapper>
         <DateText>2023.06.11</DateText>
         <PurchaseText>구매 확정</PurchaseText>
-        <RecContent src={Pancake} />
+        <PurchaseDiv>
+          <RecContent src={Pancake} />
+          <RecText>
+            [청수당]
+            <br />
+            <br />
+            수플레 팬케이크 2set
+            <br />
+            <br />
+            수량: 1개
+            <br />
+            <br />
+            8,400원
+          </RecText>
+        </PurchaseDiv>
+        <PurchaseBut>후기 작성</PurchaseBut>
       </BodyContent>
       <Footer />
     </>
